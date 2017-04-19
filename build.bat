698606fe5db377fa3c6fb76828d530adaa523494
Marcus Mathiassen <mathiassenmarcus@gmail.com>
2017-04-19 23:50:25 +0200

    Runs on WIN

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set output=MM.exe
+set output=app.exe
 del %output%
-cl /EHsc -W3 -Fe%output% *.cpp opengl32.lib glew32s.lib glfw3dll.lib
+cl /EHsc -W3 -Fe%output% .\src\*.cpp opengl32.lib glew32s.lib glfw3dll.lib
 del *.obj
